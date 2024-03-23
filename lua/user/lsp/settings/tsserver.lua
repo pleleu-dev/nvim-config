abd1bbf2563530dde107fe3dd5c7a6df7564cede
P. Leleu <pleleu.dev@gmail.com>
2024-03-23 21:31:37 +0100

    Lsp setup

---
+++ lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,22 @@
+
+local on_attach = function(client, bufnr)
+  -- format on save
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("Format", { clear = true }),
+      buffer = bufnr,
+      callback = function() vim.lsp.buf.formatting_seq_sync() end
+    })
+  end
+end
+
+
+
+return {
+   cmd = { "typescript-language-server", "--stdio" },
+   on_attach = on_attach,
+  	filetypes = {
+    "javascript",
+    "typescript", "typescriptreact", "typescript.tsx"
+  },
+}
